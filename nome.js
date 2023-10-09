c8740f22b4280fb54057c81865dfb6e0520cb864
felixrai <133838065+felixrai@users.noreply.github.com>
2023-10-09 11:06:57 -0300

    Add files via upload

---
+++ nome.js
@@ -0,0 +1,13 @@
+function separarNomeSobrenome() {
+    
+    var nomeCompleto = document.getElementById('nomeCompleto').value;
+
+    var partesNome = nomeCompleto.split(' ');
+
+    if (partesNome.length >= 2) {
+        document.getElementById('nome').value = partesNome[0];
+        document.getElementById('sobrenome').value = partesNome.slice(1).join(' ');
+    } else {
+        alert("Digite um nome completo válido (com pelo menos nome e sobrenome).");
+    }
+}
